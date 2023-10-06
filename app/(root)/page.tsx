9f0e258db00447a402dace8ff5fca7002dd40836
sahuljr7 <sahulkumarparida@gmail.com>
2023-10-06 13:57:38 +0530

    typescript

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { fetchPosts } from "@/lib/actions/thread.actions";
+
+export default async function Home() {
+
+  const result = await fetchPosts();
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
     </>
   )
 }
+
+
